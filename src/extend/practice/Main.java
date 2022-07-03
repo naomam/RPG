ccf0d20866a2872a36cac57e324de3e6ca0ad1b0
Naoki Uchida <106541486+naomam@users.noreply.github.com>
2022-07-03 18:52:11 +0900

    mainメソッド修正

--- src/extend/practice/Main.java
+++ src/extend/practice/Main.java
@@ -1,23 +1,30 @@
 package extend.practice;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args, String Brave, String Wizard, String Fighter, String Slime, String Oak, String Dragon) {
-		List<String> Humans = new ArrayList<>();
-		Humans.add(Brave);
-		Humans.add(Wizard);
-		Humans.add(Fighter);
+	public static void main(String[] args) {
 		
-		List<String> Monsters = new ArrayList<>();
-			Monsters.add(Slime);
-			Monsters.add(Oak);
-			Monsters.add(Dragon);
+		Human brave = new Brave("勇者",120,60);
+		Human wizard = new Wizard("魔法使い",100,70);
+		Human fighter = new Fighter("戦士",100,50);
+		Monster slime = new Slime("スライム",80,30);
+		Monster oak = new Slime("オーク",100,50);
+		Monster dragon = new Slime("ドラゴン",200,100);
+		
+		Human<Living> Humans = new ArrayList<>();
+			Humans.add(brave);
+			Humans.add(wizard);
+			Humans.add(fighter);
+		
+		Monster<Living> Monsters = new ArrayList<>();
+			Monsters.add(slime);
+			Monsters.add(oak);
+			Monsters.add(dragon);
 	
 			while(true) {
-				Humans.attack(Monsters);
+				Living.attack(Monsters);
 				
 			}
 	}
